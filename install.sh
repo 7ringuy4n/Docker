18033c56cdabb7b71e8cb1ff136c35384f3f9087
johnninja54 <131328514+johnninja54@users.noreply.github.com>
2023-05-26 11:34:53 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+apt-get update
+apt-get install \
+   ca-certificates \
+   curl \
+   gnupg \
+   apt-transport-https \
+   software-properties-common
+   
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+ "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+ "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+ tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl enable docker.service
+systemctl enable containerd.service
+chmod 666 /var/run/docker.sock
+usermod -aG docker $(id -u):$(id -g)
